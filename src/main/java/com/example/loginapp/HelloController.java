68e5f8fea65d2b3574bb63106754efb7dd9e1cd1
Joshored <jorgelpr.lh@gmail.com>
2025-10-26 20:59:46 -0600

    Corregi algunos errores en el codigo

--- src/main/java/com/example/loginapp/HelloController.java
+++ src/main/java/com/example/loginapp/HelloController.java
@@ -1,24 +1,48 @@
 package com.example.loginapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Button entrarBoton;
 
     @FXML
-    public void entrarBotonOnAction() {
-        entrarBoton.setText("Entrar");
-    }
-    @FXML
     private Label errorTexto;
 
-    private void entrarBotonOnAction(ActionEvent e) {
-        errorTexto.setText("Estas logeandote");
+    @FXML
+    public void entrarBotonOnAction(ActionEvent event) {
+        boolean loginValido = true;
+        if (loginValido) {
+            abrirListaClientes();
+        } else {
+            errorTexto.setText("Contraseña incorrecta o Usuario incorrecto");
+        }
     }
 
-   }
+    private void abrirListaClientes() {
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("listaClientes.fxml"));
+            Parent root = loader.load();
+
+            Stage stageTabla = new Stage();
+            stageTabla.setTitle("Lista de Clientes");
+            stageTabla.setScene(new Scene(root));
+            stageTabla.show();
+
+            Stage stageLogin = (Stage) entrarBoton.getScene().getWindow();
+            stageLogin.close();
+        } catch (IOException e) {
+            errorTexto.setText("Error al abrir lista de Clientes");
+            e.printStackTrace();
+        }
+    }
+}
