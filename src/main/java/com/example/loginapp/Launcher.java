d5ebf2ead70ad451c1b8414b3725e3ef6a666b5c
Joshored <jorgelpr.lh@gmail.com>
2025-11-12 20:35:26 -0600

    primer aproximado del dashboard

--- src/main/java/com/example/loginapp/Launcher.java
+++ src/main/java/com/example/loginapp/Launcher.java
@@ -4,11 +4,8 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Inicializar la base de datos antes de lanzar la aplicación
+       
         System.out.println("Inicializando base de datos...");
-
-        // Esta línea inicializará la base de datos automáticamente
-        // a través del bloque static en DatabaseConfigtg
 
         Application.launch(HelloApplication.class, args);
     }
