a936200f0809b08048dbc86c5fd5da65004c5394
Joshored <jorgelpr.lh@gmail.com>
2025-11-22 20:05:56 -0600

    Aumento de tamaño de letra

--- src/main/java/com/example/loginapp/Launcher.java
+++ src/main/java/com/example/loginapp/Launcher.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-       
+        //
         System.out.println("Inicializando base de datos...");
 
         Application.launch(HelloApplication.class, args);
